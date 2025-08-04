6166c17bd63d79099274d14a980c03d4607fa188
Shoaib Ul Hassan <shoaib@example.com>
2025-08-04 09:28:27 +0500

    Added new files (e.g. scene.bin and github.png)

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
       <div className='bg-hero-pattern bg-cover
        bg-no-repeat bg-center'>
         <Navbar/>
-        {/* <Hero/> */}
+        <Hero/>
       </div>
-      {/* <About/> */}
+      <About/>
       {/* <Experience/> */}
       {/* <Tech/> */}
       {/* <Works/> */}
--- src/components/components/canvas/Computers.jsx
+++ src/components/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import { OrbitControls,Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Computers = ({ isMobile}) => {

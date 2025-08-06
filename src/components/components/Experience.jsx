882b70e0c02927b94147af5eb4f9290f5a88697d
Shoaib Ul Hassan <shoaibulhasan556@gmail.com>
2025-08-06 15:47:53 +0500

    built experiece.jsx

--- src/components/components/Experience.jsx
+++ src/components/components/Experience.jsx
@@ -7,17 +7,39 @@ import {styles} from '@/style';
 import {experiences} from '@/constants';
 import {SectionWraper} from '../../hoc';
 import {textVariant} from '@/utlis/motion';
-// import Background from 'three/src/renderers/common/Background.js';
+import Background from 'three/src/renderers/common/Background.js';
 
 const ExperienceCard = ({experience}) =>(
   <VerticalTimelineElement
-  contentStyle={{Background:'#1d1836',color:
-  '#fff'}}
-  // contentArrayStyle={{borderRight:'7px solid
-  // #232631'}}
-  // date={{}}
+  contentStyle={{background:'#1d1836', color: '#fff'}}
+  contentArrowStyle={{borderRight:'7px solid #232631'}}
+  date={experience.date}
+  iconStyle={{background: experience.iconBg}}
+  icon={
+    <div className='flex justify-center items-center
+    h-full w-full'>
+      <img src={experience.icon} 
+      alt={experience.company_name}
+      className='w-[60%] h-[60%] object-contain' />
+    </div>
+  }
   >
-    
+    <div>
+      <h3 className='text-white text-24px 
+      font-bold'>{experience.title}</h3>
+      <p className='text-secondary text-[24px]
+      font-semibold' style={{margin:0}}>
+        {experience.company_name}</p>
+    </div>
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point, index) =>(
+        <li key={`experience-point-${index}`}
+        className="text-white-100 text-[14px]
+        pl-1 tracking-wider">
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 )
 const Experience = () => {

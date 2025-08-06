34595cbabd502bdd2a818bf38a9e7296d86e72d3
Shoaib Ul Hassan <shoaibulhasan556@gmail.com>
2025-08-06 11:47:47 +0500

    Mdel Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Hero/>
       </div>
       <About/>
-      {/* <Experience/> */}
+      <Experience/>
       {/* <Tech/> */}
       {/* <Works/> */}
       {/* <Feedbacks/> */}
